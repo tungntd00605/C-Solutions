e0a184a0190933d8a297566c6a4b19039c0c3cce
Your Name <you@example.com>
2019-01-09 22:54:05 +0700

    Bai thuc hanh EAP phan 2

---
+++ EAPExam2/EAPExam2/Controllers/HomeController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EAPExam2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace EAPExam2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration;
+        public HomeController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            var uploadSuccess = false;
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length <= 0)
+                {
+                    continue;
+                }
+
+                // NOTE: uncomment either OPTION A or OPTION B to use one approach over another
+
+                // OPTION A: convert to byte array before upload
+                //using (var ms = new MemoryStream())
+                //{
+                //    formFile.CopyTo(ms);
+                //    var fileBytes = ms.ToArray();
+                //    uploadSuccess = await UploadToBlob(formFile.FileName, fileBytes, null);
+
+                //}
+
+                // OPTION B: read directly from stream for blob upload      
+                using (var stream = formFile.OpenReadStream())
+                {
+                    uploadSuccess = await UploadToBlob(formFile.FileName, null, stream);
+                }
+
+            }
+
+            if (uploadSuccess)
+            {
+                return File(files[0].OpenReadStream(), "image/jpeg");
+            }
+            else {
+                return File(files[0].OpenReadStream(), "image/jpeg");
+            }
+                
+        }
+
+        private async Task<bool> UploadToBlob(string filename, byte[] imageBuffer = null, Stream stream = null)
+        {
+            CloudStorageAccount storageAccount = null;
+            CloudBlobContainer cloudBlobContainer = null;
+            string storageConnectionString = _configuration["storageconnectionstring"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Create the CloudBlobClient that represents the Blob storage endpoint for the storage account.
+                    CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                    // Create a container called 'uploadblob' and append a GUID value to it to make the name unique. 
+                    cloudBlobContainer = cloudBlobClient.GetContainerReference("uploadblob" + Guid.NewGuid().ToString());
+                    await cloudBlobContainer.CreateAsync();
+
+                    // Set the permissions so the blobs are public. 
+                    BlobContainerPermissions permissions = new BlobContainerPermissions
+                    {
+                        PublicAccess = BlobContainerPublicAccessType.Blob
+                    };
+                    await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(filename);
+
+                    if (imageBuffer != null)
+                    {
+                        // OPTION A: use imageBuffer (converted from memory stream)
+                        await cloudBlockBlob.UploadFromByteArrayAsync(imageBuffer, 0, imageBuffer.Length);
+                    }
+                    else if (stream != null)
+                    {
+                        // OPTION B: pass in memory stream directly
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }
+                catch (StorageException ex)
+                {
+                    return false;
+                }
+                finally
+                {
+                    // OPTIONAL: Clean up resources, e.g. blob container
+                    //if (cloudBlobContainer != null)
+                    //{
+                    //    await cloudBlobContainer.DeleteIfExistsAsync();
+                    //}
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
